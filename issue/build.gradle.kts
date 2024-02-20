e47128a291b4bab58c8b2d81201b33455ace912c
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-02-20 13:27:58 -0500

    Add task input based reproducer

--- issue/build.gradle.kts
+++ issue/build.gradle.kts
@@ -1,28 +1,20 @@
 import org.gradle.api.internal.provider.DefaultProvider
-import org.gradle.tooling.events.FinishEvent
-import org.gradle.tooling.events.OperationCompletionListener
 
 val readFileFun = DefaultProvider {
     val testFile = layout.projectDirectory.file("test.txt").asFile
     { testFile.exists() }
 }
 
-abstract class MyListener : BuildService<MyListener.Params>, OperationCompletionListener {
-    interface Params : BuildServiceParameters {
-        val readFileFun: Property<() -> Boolean>
+abstract class MyTask : DefaultTask() {
+    @get:Input
+    abstract val readFileFunProp: Property<() -> Boolean>
+
+    @TaskAction
+    fun run() {
+        println("file exists: ${readFileFunProp.get().invoke()}")
     }
-
-    override fun onFinish(event: FinishEvent) {
-        println("file exists: ${parameters.readFileFun.get().invoke()}")
-    }
 }
 
-val myListener = project.gradle.sharedServices.registerIfAbsent("myListener", MyListener::class.java) {
-    parameters.readFileFun.set(readFileFun)
+tasks.register<MyTask>("myTask") {
+    readFileFunProp.set(readFileFun)
 }
-
-interface BuildEventsHolder {
-    @get:Inject
-    val buildEventsListenerRegistry: BuildEventsListenerRegistry
-}
-objects.newInstance(BuildEventsHolder::class).buildEventsListenerRegistry.onTaskCompletion(myListener)
